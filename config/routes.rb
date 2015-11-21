55b0841ea0aaffcf7656476bf55e11e909641566
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-21 00:31:10 -0800

    nested routes, added some views from the beginning

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root 'walkers#start'
 
-  resources :walkers
+  resources :walkers do
+    resources :trips do
+      resources :turns
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
