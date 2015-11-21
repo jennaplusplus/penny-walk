5949aca415812a72d5f4907fa954f479f16becb1
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-11-21 01:41:41 -0800

    Adds Turn, Trip, Walker seeds for database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+walkers = [
+  {name: "Ann"},
+  {name: "Bob"},
+  {name: "Zoe"},
+  {name: "Matthew"}
+]
+
+turns = [
+  {left_right: 1, blocks: 1, trip_id: 1, sequence: 1},
+  {left_right: 0, blocks: 2, trip_id: 2, sequence: 1},
+  {left_right: 0, blocks: 2, trip_id: 1, sequence: 2},
+  {left_right: 1, blocks: 3, trip_id: 2, sequence: 3},
+  {left_right: 1, blocks: 3, trip_id: 1, sequence: 3},
+  {left_right: 0, blocks: 1, trip_id: 2, sequence: 2},
+  {left_right: 0, blocks: 2, trip_id: 1, sequence: 4},
+  {left_right: 1, blocks: 3, trip_id: 2, sequence: 1},
+  {left_right: 0, blocks: 2, trip_id: 1, sequence: 5},
+  {left_right: 1, blocks: 1, trip_id: 1, sequence: 3}
+]
+
+trips = [
+        {
+          start_latitude: 37.3717427,
+          start_longitude: -122.019002,
+          end_latitude: 37.3843212,
+          end_longitude: -122.1022363,
+          walker_id: 1
+        },
+        {
+          start_latitude: 37.3717427,
+          start_longitude: -122.019002,
+          end_latitude: 37.3717420,
+          end_longitude: -122.1022370,
+          walker_id: 1
+        },
+        {
+          start_latitude: 47.665133,
+          start_longitude: -122.354170,
+          end_latitude: 47.686811,
+          end_longitude: -122.336564,
+          walker_id: 2
+        },
+        {
+          start_latitude: 51.5189606,
+          start_longitude: -0.2199356,
+          end_latitude: 51.4985539,
+          end_longitude: -0.1323883,
+          walker_id: 3
+        }
+]
+
+walkers.each do |w|
+  Walker.create w
+end
+
+turns.each do |turn|
+  Turn.create turn
+end
+
+trips.each do |trip|
+  Trip.create trip
+end
