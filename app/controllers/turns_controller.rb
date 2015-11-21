f440688a0c1530a6045215e387eebe130a5b0be4
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-21 04:06:20 -0800

    WORKING SOMETHING

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,5 +1,31 @@
 class TurnsController < ApplicationController
   def show
     @turn = Turn.find(params[:id])
+    if @turn.left_right == true
+      @direction = "right"
+    else
+      @direction = "left"
+    end
+  end
+
+  def generate_left_right
+    direction = rand(2)
+    case direction
+    when 0
+      return false
+    when 1
+      return true
+    end
+  end
+
+  def generate_blocks
+    blocks = rand(1..3)
+    return blocks
+  end
+
+  def create
+    prev_seq = Turn.last.sequence
+    turn = Turn.create(left_right: generate_left_right, blocks: generate_blocks, trip_id: params[:trip_id], sequence: prev_seq + 1)
+    redirect_to walker_trip_turn_path(id: turn.id, trip_id: turn.trip_id)
   end
 end
