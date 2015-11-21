96c8cecb25b0edcffb02bb03da4523266a680505
Lauren Granger <lr.granger21@gmail.com>
2015-11-21 07:17:35 -0800

    app generated data to (x,y) coordinates wtested -- works

--- vis_logic.rb
+++ vis_logic.rb
@@ -1,50 +1,61 @@
 #this logic takes the L/R, # of blocks, and sequence data from the turns model and outputs a html canvas document.
 
+left_right = [nil, false, false, true, false, true, true]
+blocks = [3, 1, 3, 1, 2, 3, 1]
+sequence = [0, 1, 2, 3, 4, 5, 6]
+
 # data here is the left_right, blocks, & sequence in arrays of those data
 def generate_cartesian(left_right, blocks, sequence)
+  x = 0
+  y = 0
   x_array = []
   y_array = []
-  sequence.each do
-    if sequence == 0
-      y = blocks[sequence]
+  sequence.each do |seq|
+    if seq == 0
+      y = blocks[seq]
       x_array.push(0)
       y_array.push(y)
-    elsif sequence == 1
-      if left_right == "right"
-        x += blocks[sequence]
+    elsif seq == 1
+      if left_right[seq] == true
+        x += blocks[seq]
       else
-        x -= blocks[sequence]
+        x -= blocks[seq]
       end
       x_array.push(x)
       y_array.push(y)
-    elsif sequence % 2 == 0 #if sequence step is even
-      if left_right == "left"
+    elsif seq % 2 == 0 #if seq step is even
+      if left_right[seq] == true
         if x_array[-1] > x_array[-2]
-          y -= blocks[sequence]
+          y -= blocks[seq]
         else
-          y += blocks[sequence]
+          y += blocks[seq]
+        end
       else #if left_right is "right"
         if x_array[-1] > x_array[-2]
-          y += blocks[sequence]
+          y += blocks[seq]
         else
-          y -= blocks[sequence]
+          y -= blocks[seq]
+        end
       end
       x_array.push(x)
       y_array.push(y)
-    else #if sequence step is odd
-      if left_right == "left"
+    else #if seq step is odd
+      if left_right[seq] == false
         if y_array[-1] > y_array[-2]
-          x -= blocks[sequence]
+          x -= blocks[seq]
         else
-          x += blocks[sequence]
+          x += blocks[seq]
+        end
       else #if left_right is "right"
         if y_array[-1] > y_array[-2]
-          x += blocks[sequence]
+          x += blocks[seq]
         else
-          x -= blocks[sequence]
+          x -= blocks[seq]
+        end
       end
       x_array.push(x)
       y_array.push(y)
     end
   end
+  return x_array, y_array
 end
